2bc0916638fc684099a65c61c362964412cf1093
Raul Acosta <107330142+racodess@users.noreply.github.com>
2024-02-20 03:03:58 -0500

    Add working calls to bubbleSort for each algo

--- operating-systems/scheduling.c
+++ operating-systems/scheduling.c
@@ -52,26 +52,18 @@ int main(int argc, char **argv) {
 
 	if (strcmp(FCFS, algo) == 0){
 		printf("\nSuccess for FCFS!\n");
+
+		bubbleSort(arrival, num_process);
 	}
 	else if (strcmp(SJF, algo) == 0){
 		printf("\nSuccess for SJF!\n");
+
+		bubbleSort(burst, num_process);
 	}
 	else {
 		printf("Not working....");
 	}
 
-	// Test bubbleSort
-  int arr[] = {64, 34, 25, 12, 22, 11, 90};
-	int n = sizeof(arr)/sizeof(arr[0]);
-	bubbleSort(arr, n);
-	printf("Sorted array: \n");
-	for (int i=0; i<n; i++)
-		printf("%d ", arr[i]);
-	printf("\n");
-  // End Test bubbleSort
-
-	
-	
 	// And remember to release the dynamically allocated memory after using
 	free(arrival);
 	free(burst);
