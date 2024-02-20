f71f4c517b26d398fee1a41c8f5be5acc9f1fa1f
Raul Acosta <107330142+racodess@users.noreply.github.com>
2024-02-19 22:49:55 -0500

    Add description, Add main() accepting CLI args

---
+++ operating-systems/scheduling.c
@@ -0,0 +1,21 @@
+/* Raul Acosta (ID: 4566606)
+ *
+ * This program implements FCFS and SJF scheduling
+ * algorithms in C and calculates the average waiting
+ * time and average turnaround time of concurrent processes.
+ *
+ * It expects a text file and scheduling algorithm (FCFS or SJF)
+ * as arguments. Inputs are the number of processes, arrival time,
+ * and burst time. Outputs are the order of process execution,
+ * average waiting time, and average turn around time.
+ */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	if (argc != 2)
+		printf("Error: two arguments are required.\n");
+
+	return 0;
+}
+
