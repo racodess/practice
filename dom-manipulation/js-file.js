01268fa097bd055e2328444340b56cc7a6aecfe9
Raul Acosta <107330142+racodess@users.noreply.github.com>
2024-02-24 15:22:46 -0500

    Add div to container with children h1 and p

--- dom-manipulation/js-file.js
+++ dom-manipulation/js-file.js
@@ -16,3 +16,20 @@ const h3 = document.createElement('h3');
 h3.textContent = "I'm a blue h3!";
 h3.style.color = 'blue';
 body.insertBefore(h3, container);
+
+const content2 = document.createElement('div');
+content2.classList = 'content2';
+content2.style.height = '100px';
+content2.style.width = '500px';
+content2.style.border = '5px solid black';
+content2.style.backgroundColor = 'pink';
+container.appendChild(content2);
+
+const contentHeader = document.createElement('h1');
+contentHeader.textContent = "I'm in a div";
+
+const contentPara = document.createElement('p');
+contentPara.textContent = "ME TOO!"
+
+content2.appendChild(contentHeader);
+content2.appendChild(contentPara);
