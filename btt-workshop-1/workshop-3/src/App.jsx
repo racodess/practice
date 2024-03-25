b739f988bf3b741b6b428f4b855a0f1ad7b91acc
Raul Acosta <107330142+racodess@users.noreply.github.com>
2024-03-25 19:43:19 -0400

    Add workshop-3 directory

---
+++ btt-workshop-1/workshop-3/src/App.jsx
@@ -0,0 +1,42 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div id="container">
+      <header>
+        <nav>
+          <a href="">About Me</a>
+          <a href="">Projects</a>
+          <a href="">Contacts</a>
+        </nav>
+      </header>
+
+      <div id="center">
+        <div id="inner">
+          <div id="left">
+            <h2>Hi There!</h2>
+            <h2>My Name's Raul.</h2>
+            <div id="roles">
+              <p>Student</p>
+              <p>Not a Web-Dev Instructor</p>
+              <p>Former-nurse</p>
+              <p>Front-end</p>
+              <p>Back-end</p>
+              <p>Incoming AMEX 2024</p>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi minima magnam similique perferendis. Quam maiores ratione modi. Recusandae eligendi quas eius laboriosam, blanditiis aut sequi tenetur consequatur sunt cum ut.</p>
+          </div>
+          <div id="right">
+            <img src="https://st3.depositphotos.com/9998432/13335/v/450/depositphotos_133352010-stock-illustration-default-placeholder-man-and-woman.jpg"></img>
+          </div>
+        </div>
+      </div>
+      <footer></footer>
+    </div>
+    </>
+  )
+}
+
+export default App
