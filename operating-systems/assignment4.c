887c2a3506813514945122a3615ffdebbbdf5a56
Raul Acosta <107330142+racodess@users.noreply.github.com>
2024-03-29 18:44:07 -0400

    Add comments

--- operating-systems/assignment4.c
+++ operating-systems/assignment4.c
@@ -2,7 +2,7 @@
  *
  * This program implements a fair starvation and deadlock-free solution
  * to the readers-writers problem by ensuring max reader concurrency and
- * preventing starvation of processes. 
+ * preventing starvation of processes. It is implemented using semaphores.
  */
 
 #include <stdio.h>
@@ -11,39 +11,46 @@
 #include <semaphore.h>
 #include <unistd.h>
 
+// Define maximum number of times readers access shared value counter.
 #define MAX_READS 2000000
 
-sem_t ok_to_read;
+// Declare semaphores
+sem_t ok_to_read; 
 sem_t ok_to_write;
-sem_t protect;
+sem_t protect; // Protects readers counter.
 
-int readers = 0;
-int shared_value = 0;
+int readers = 0; // Keeps track of active readers.
+int shared_value = 0; // Counter written by writer() and accessed by reader().
 
+// Reader function, accepts index i from reader's pthread_create for loop to identify threads. Also returns NULL to avoid warnings on compile.
 void *reader(void *arg){
-  long reader_thread = (long)arg;
+  long reader_thread = (long)arg; // Thread id.
 
+  // Allows readers to access shared value counter 2000000 times.
   for (int i = 0; i < MAX_READS; i++){
-    sem_wait(&ok_to_read);
+    sem_wait(&ok_to_read); // Ensures it is safe to read.
 
-    sem_wait(&protect);
-    readers++;
+    sem_wait(&protect); // Protects readers counter.
+    readers++; // Increment for new active reader.
 
+    // If an active reader is present, wait to write.
     if (readers == 1){
-      sem_wait(&ok_to_write);
+      sem_wait(&ok_to_write); // Ensures it is safe to write.
     }
     sem_post(&protect);
 
     sem_post(&ok_to_read);
 
     sem_wait(&protect);
-    readers--;
+    readers--; // Decrement when active reader leaves.
 
+    // When all active readers leave, signal it is ok to write.
     if (readers == 0){
       sem_post(&ok_to_write);
     }
     sem_post(&protect);
 
+    // When 2000000 accesses complete, identify reader and counter value.
     if (i == MAX_READS - 1){
       printf("I'm reader%ld, counter = %d\n", reader_thread, shared_value);
     }
@@ -52,23 +59,29 @@ void *reader(void *arg){
   return NULL;
 }
 
+// Accepts void *arg, and returns NULL to prevent compilation warnings.
 void *writer(void *arg){
   printf("Executing... please wait...\n");
+  
+  // Writes 25000 times.
   for (int i = 0; i < 25000; i++){
     sem_wait(&ok_to_write);
 
     shared_value++;
 
-    sem_post(&ok_to_write);
+    sem_post(&ok_to_write); 
   }
+
   printf("Writer Done!\n");
 
   return NULL;
 }
 
+// Accepts arguments from command line
 int main(int argc, char *argv[]) {
-  int input = atoi(argv[1]);
+  int input = atoi(argv[1]); // Turns user's reader thread number input to int.
 
+  // CLI argument input validation.
   if (input > 12){
     printf("Warning: The maximum number of readers allowed is 12.\nInput: %d, but this program will proceed with only 12 readers and 1 writer\n", input);
     input = 12;
@@ -81,11 +94,13 @@ int main(int argc, char *argv[]) {
   //Add 1 for writer thread.
   input++;
 
+  // Initialize semaphores.
   sem_init(&ok_to_read, 0, 1);
   sem_init(&ok_to_write, 0, 1);
   sem_init(&protect, 0, 1);
 
 
+  // Declare pthread_t array for dynamic thread creation, rc for error handling. Source: Dr. Kaoutar Ben Ahmend, canvas
   pthread_t tid[input];
   int rc;
 
@@ -102,7 +117,7 @@ int main(int argc, char *argv[]) {
   }
 
   for (long i = 1; i < input; i++){
-    if ((rc = pthread_create(&tid[i], &attr, reader, (void *)i))) {
+    if ((rc = pthread_create(&tid[i], &attr, reader, (void *)i))) { // Pass index cast to void *, allows for thread identification in reader function.
         fprintf(stderr, "ERROR: pthread_create, rc: %d\n", rc);
         exit(0);
     }
