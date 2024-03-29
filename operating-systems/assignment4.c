8f87b9747f3ff29a32f4958538f8f6f97bf39e19
Raul Acosta <107330142+racodess@users.noreply.github.com>
2024-03-29 14:59:10 -0400

    Add cli input validation and thread create logic

--- operating-systems/assignment4.c
+++ operating-systems/assignment4.c
@@ -16,7 +16,7 @@
 
 /* Struct a shared variable to store result */
 struct shared_data {
-    int value;  
+  int value;  
 };
 
 /* Global shared variable */
@@ -26,56 +26,70 @@ pthread_mutex_t mutex;
 
 /* Thread1 function */
 void *thread1() {
-    }
+  }
     
-    return NULL;
+  return NULL;
 }
 
 int main(int argc, char *argv[]) {
-    pthread_t tid[2CHANGGE THIS CHANGE THIS CHANGE THIS];
-    int rc;
+  int input = atoi(*argv[1]);
 
-    /* Allocate memory for shared data */
-    counter = (struct shared_data *) malloc(sizeof(struct shared_data));
-    counter->value = 0;
+  if (input > 12){
+    printf("Warning: The maximum number of readers allowed is 12.\n Input: %d, but this program will proceed with only 12 readers and 1 writer\n", input);
+    input = 12;
+  }
+  else if (input < 0 || input == null){
+    printf("Error: Number of readers cannot be less than 0 or null"); 
+    exit(0);
+  }
 
-    /*Initialize semaphores
-     * change
-     * change
-     * change
-     * change
-     * change
-     * change
-     * change*/
-    if ((pthread_mutex_init(&mutex, NULL))) {
-        printf("Error occured when initialize mutex lock.");
-        exit(0);
-    }
+  //Add 1 for the writer thread
+  input += 1;
 
-    pthread_attr_t attr;
-    if ((pthread_attr_init(&attr))) {
-        printf("Error occured when initialize pthread_attr_t.");
-        exit(0);
-    }
+  pthread_t tid[input];
+  int rc;
 
-    /* Create thread1 */
-    if ((rc = pthread_create(&tid[0], &attr, CHANGETHISthreeeeeeeeeeeeeeeeeeeead1, NULL))) {
+  /* Allocate memory for shared data */
+  counter = (struct shared_data *) malloc(sizeof(struct shared_data));
+  counter->value = 0;
+
+  /*Initialize semaphores
+    * change
+    * change
+    * change
+    * change
+    * change
+    * change
+    * change*/
+  if ((pthread_mutex_init(&mutex, NULL))) {
+      printf("Error occured when initialize mutex lock.");
+      exit(0);
+  }
+
+  pthread_attr_t attr;
+  if ((pthread_attr_init(&attr))) {
+      printf("Error occured when initialize pthread_attr_t.");
+      exit(0);
+  }
+
+  /* Create threads */
+  for (int i = 0; i < input; i++){
+    if ((rc = pthread_create(&tid[i], &attr, thread1, NULL))) {
         fprintf(stderr, "ERROR: pthread_create, rc: %d\n", rc);
         exit(0);
     }
+  }
 
-    /* Wait for threads to finish */
-    pthread_join(tid[0], NULL);
-    pthread_join(tid[1], NULL);
+  /* Wait for threads to finish */
+  for (int i = 0; i < input; i++){
+    pthread_join(tid[i], NULL);
+  }
 
-		printf("from parent counter = %d\n", counter->value);
+  /* Clean up */
+  pthread_mutex_destroy(&mutex); //CHAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAANGE!!!
+  free(counter); //CHAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAANGE!!!
+  pthread_exit(NULL);
 
- 
-    /* Clean up */
-    pthread_mutex_destroy(&mutex);
-    free(counter);
-    pthread_exit(NULL);
-
-    return 0;
+  return 0;
 }
 
