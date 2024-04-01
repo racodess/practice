9255c79940e0aeac10f9a69117c3708219758cf6
Raul Acosta <107330142+racodess@users.noreply.github.com>
2024-04-01 19:38:50 -0400

    Add Form tag, import Form from Form.jsx

--- btt-workshop-1/workshop-4/project5/project5/src/App.jsx
+++ btt-workshop-1/workshop-4/project5/project5/src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from "react";
+import Form from "./components/Form.jsx";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Form />
+    </>
+  );
 }
 
 export default App;
