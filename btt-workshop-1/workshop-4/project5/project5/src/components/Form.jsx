e1a4527d30aa51b159790b784bdeb18814e92078
Raul Acosta <107330142+racodess@users.noreply.github.com>
2024-04-01 20:37:31 -0400

    Add questions object array, form tag in div

--- btt-workshop-1/workshop-4/project5/project5/src/components/Form.jsx
+++ btt-workshop-1/workshop-4/project5/project5/src/components/Form.jsx
@@ -1,5 +1,28 @@
 const Form = () => {
-  return <div>Form</div>;
+  const questions = [
+    {
+      id: 10001,
+      question: "What fruit is yellow",
+      answers: ["Apple", "Orange", "Lemons"],
+    },
+    {
+      id: 10002,
+      question: "What color is best?",
+      answers: ["Yellow", "Green", "Blue", "Cyan"],
+    },
+  ];
+
+  return (
+    <div>
+      <form>
+        <h1>The Quiz</h1>
+        {questions.map((question) => {
+          console.log(question.id);
+          return "";
+        })}
+      </form>
+    </div>
+  );
 };
 
 export default Form;
