5b39b114703b00459d10cb9bc8743ac6506e1eef
Raul Acosta <107330142+racodess@users.noreply.github.com>
2024-04-03 18:34:06 -0400

    Add questions object with properties, id, question, answers

--- btt-workshop-1/workshop-4/project5/project5/src/components/Form.jsx
+++ btt-workshop-1/workshop-4/project5/project5/src/components/Form.jsx
@@ -1,4 +1,16 @@
 const Form = () => {
+  const questions = [
+    {
+      id: 1001,
+      question: "Which fruit is yellow?",
+      answers: ["Apple", "Orange", "Lemon", "Starberry"],
+    },
+    {
+      id: 1002,
+      question: "Which fruit is red?",
+      answers: ["Apple", "Orange", "Lemon", "Starberry"],
+    },
+  ];
   return <div>Form</div>;
 };
 
