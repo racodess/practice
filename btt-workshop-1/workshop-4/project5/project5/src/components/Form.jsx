7c30b7abd40a724b2089dd45651514b5980ea1de
Raul Acosta <107330142+racodess@users.noreply.github.com>
2024-04-03 19:05:37 -0400

    Change question key to questionTitle, Add logic for returning question divs

--- btt-workshop-1/workshop-4/project5/project5/src/components/Form.jsx
+++ btt-workshop-1/workshop-4/project5/project5/src/components/Form.jsx
@@ -2,21 +2,23 @@ const Form = () => {
   const questions = [
     {
       id: 1001,
-      question: "Which fruit is yellow?",
+      questionTitle: "Which fruit is yellow?",
       answers: ["Apple", "Orange", "Lemon", "Starberry"],
     },
     {
       id: 1002,
-      question: "Which fruit is red?",
+      questionTitle: "Which fruit is red?",
       answers: ["Apple", "Orange", "Lemon", "Starberry"],
     },
   ];
 
-  const array = [1, 2, 3, 4];
-
   return (
     <div>
       <h1>The Quiz</h1>
+
+      {questions.map((question) => {
+        return <div key={questions.id}>{question.questionTitle}</div>;
+      })}
     </div>
   );
 };
