238830e11aec88a6c7b3a63f604a8132f20d4bcf
Raul Acosta <107330142+racodess@users.noreply.github.com>
2024-04-03 18:28:37 -0400

    Remove questions object

--- btt-workshop-1/workshop-4/project5/project5/src/components/Form.jsx
+++ btt-workshop-1/workshop-4/project5/project5/src/components/Form.jsx
@@ -1,17 +1,4 @@
 const Form = () => {
-  const questions = [
-    {
-      id: 10001,
-      question: "What fruit is yellow",
-      answers: ["Apple", "Orange", "Lemons"],
-    },
-    {
-      id: 10002,
-      question: "What color is best?",
-      answers: ["Yellow", "Green", "Blue", "Cyan"],
-    },
-  ];
-
   return <div>Form</div>;
 };
 
