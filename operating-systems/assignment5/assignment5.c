f9b688b1d2079d9f777348c98371caeeb2d92942
Raul Acosta <107330142+racodess@users.noreply.github.com>
2024-04-09 01:32:35 -0400

    Add loop for reading and storing page references from file

--- operating-systems/assignment5/assignment5.c
+++ operating-systems/assignment5/assignment5.c
@@ -11,5 +11,11 @@ int main(int argc, char *argv[]){
   int numFrames = atoi(argv[2]);
 
   FILE *pageRefFile = fopen(argv[1], "r");
+
+  int numPageRefs = 0;
+  int pageRefs[100];
+  while (fscanf(pageRefFile, "%d", &pageRefs[numPageRefs]) != EOF)
+    numPageRefs++;
+
   fclose(pageRefFile);
 }
