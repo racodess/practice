24cd8b926e63da9b155a6ec50bb0b7eb122a5f44
Raul Acosta <107330142+racodess@users.noreply.github.com>
2024-06-25 17:40:38 -0400

    Fix logic error causing failed testcases

--- grokking-data-structures/src/NextGreaterElement.java
+++ grokking-data-structures/src/NextGreaterElement.java
@@ -19,37 +19,50 @@ public class NextGreaterElement {
 
 
     public List<Integer> nextLargerElement(List<Integer> arr) {
-        List<Integer> res = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
         Stack<Integer> stack = new Stack<>();
 
-        for(int i = 0; i < arr.size(); i++){
-            for(int j = arr.size() - 1; j >= 0 ; j--) {
+        for (int i = 0; i < arr.size(); i++){
+            for (int j = arr.size() - 1; j >= i + 1; j--) {
                 stack.push(arr.get(j));
             }
 
-            while(!stack.empty()){
-                int val = stack.pop();
+            if (stack.empty()) {
+                result.add(-1);
+            }
+            else {
+                while (!stack.empty()) {
+                    int val = stack.pop();
 
-                if(arr.get(i) < val) {
-                    res.add(val);
-                    break;
+                    if (arr.get(i) < val) {
+                        result.add(val);
+                        break;
+                    }
+                    else if (stack.empty()) {
+                        result.add(-1);
+                    }
                 }
             }
-
-            if(stack.empty())
-                res.add(-1);
         }
 
-        return res;
+        return result;
     }
 
     void testCase(){
-        List<Integer> arr = new ArrayList<>();
+        List<Integer> arr;
 
-        for(int i = 1; i <= 5; i++){
-            arr.add(i);
-        }
+        arr = new ArrayList<>(){{add(1); add(2); add(3); add(4); add(5);}};
+        System.out.println("\nYour input: " + arr);
+        System.out.println("Output: " + nextLargerElement(arr));
 
-        System.out.println(nextLargerElement(arr));
+        arr.clear();
+        arr = new ArrayList<>(){{add(5); add(4); add(3); add(2); add(1);}};
+        System.out.println("\nYour input: " + arr);
+        System.out.println("Output: " + nextLargerElement(arr));
+
+        arr.clear();
+        arr = new ArrayList<>(){{add(1); add(3); add(5); add(2); add(4);}};
+        System.out.println("\nYour input: " + arr);
+        System.out.println("Output: " + nextLargerElement(arr));
     }
 }
