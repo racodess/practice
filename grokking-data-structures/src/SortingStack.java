dbb4115c4c5cb4d9461e37e5c950b7bc7996464d
Raul Acosta <107330142+racodess@users.noreply.github.com>
2024-06-26 13:29:32 -0400

    Refactor solution to cover edge cases

--- grokking-data-structures/src/SortingStack.java
+++ grokking-data-structures/src/SortingStack.java
@@ -12,12 +12,22 @@ public class SortingStack {
                 while(!tmp2.empty())
                     input.push(tmp2.pop());
 
-                tmp1.push(input.pop());
-                tmp2.push(input.pop());
+                do tmp1.push(input.pop());
+                while(!input.empty() && input.peek() < tmp1.peek());
+
+                if(input.empty() && tmp2.empty()){
+                    while(!tmp1.empty())
+                        tmp2.push(tmp1.pop());
+
+                    return tmp2;
+                }
+
+                do tmp2.push(input.pop());
+                while(!input.empty() && input.peek() > tmp2.peek());
             }
-            else if(input.peek() < tmp1.peek() || input.peek() < tmp2.peek())
+            else if(!input.empty() && input.peek() < tmp1.peek() || !input.empty() && input.peek() < tmp2.peek())
                 tmp1.push(input.pop());
-            else
+            else if(!input.empty())
                 tmp2.push(input.pop());
         }
 
@@ -25,14 +35,53 @@ public class SortingStack {
     }
 
     public static void main (String[] args){
-        Stack<Integer> s = new Stack<>();
+        Stack<Integer> s1 = new Stack<>();
+        Stack<Integer> s2 = new Stack<>();
+        Stack<Integer> s3 = new Stack<>();
+        Stack<Integer> s4 = new Stack<>();
+        Stack<Integer> s5 = new Stack<>();
 
-        s.push(5);
-        s.push(4);
-        s.push(3);
-        s.push(2);
-        s.push(1);
+        s1.push(5);
+        s1.push(4);
+        s1.push(3);
+        s1.push(2);
+        s1.push(1);
 
-        sortStack(s);
+        System.out.println(sortStack(s1));
+
+        s2.push(34);
+        s2.push(3);
+        s2.push(31);
+        s2.push(98);
+        s2.push(92);
+        s2.push(23);
+
+        System.out.println(sortStack(s2));
+
+        s3.push(4);
+        s3.push(3);
+        s3.push(2);
+        s3.push(10);
+        s3.push(12);
+        s3.push(1);
+        s3.push(5);
+        s3.push(6);
+
+        System.out.println(sortStack(s3));
+
+        s4.push(20);
+        s4.push(10);
+        s4.push(-5);
+        s4.push(-1);
+
+        System.out.println(sortStack(s4));
+
+        s5.push(1);
+        s5.push(2);
+        s5.push(3);
+        s5.push(4);
+        s5.push(5);
+
+        System.out.println(sortStack(s5));
     }
 }
