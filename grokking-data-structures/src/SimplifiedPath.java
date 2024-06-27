d8cfbc0968fa589a2809b44fcc3d7cdf688a4988
Raul Acosta <107330142+racodess@users.noreply.github.com>
2024-06-26 21:07:39 -0400

    Add solution for stack problem

--- grokking-data-structures/src/SimplifiedPath.java
+++ grokking-data-structures/src/SimplifiedPath.java
@@ -1,8 +1,42 @@
 import java.util.*;
 
 public class SimplifiedPath {
+
     public String simplifyPath(String path){
 
-        return "";
+        StringTokenizer tkn = new StringTokenizer(path, "/");
+        Stack<String> opr = new Stack<>();
+        while (tkn.hasMoreTokens()){
+            String tmp = tkn.nextToken();
+            if (tmp.equals("."))
+                continue;
+            else if (!tmp.equals("..")){
+                opr.push(tmp);
+            }
+            else if (!opr.empty()) {
+                opr.pop();
+            }
+        }
+
+        Stack<String> dir = new Stack<>();
+        while (!opr.empty())
+            dir.push(opr.pop());
+
+        StringBuilder newPath = new StringBuilder();
+        while (!dir.empty())
+            newPath.append("/").append(dir.pop());
+
+        if (newPath.isEmpty())
+            newPath.append("/");
+
+        return newPath.toString();
+    }
+
+    public static void main(String[] args){
+
+        SimplifiedPath test = new SimplifiedPath();
+        System.out.println(test.simplifyPath("/a//b////c/d//././/.."));
+        System.out.println(test.simplifyPath("/../"));
+        System.out.println(test.simplifyPath("/home//foo/"));
     }
 }
