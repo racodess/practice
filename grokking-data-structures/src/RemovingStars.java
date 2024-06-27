539fc60b9338b8df1e540e2a4c375fd0407cecc5
Raul Acosta <107330142+racodess@users.noreply.github.com>
2024-06-27 16:59:55 -0400

    Add solution template

---
+++ grokking-data-structures/src/RemovingStars.java
@@ -0,0 +1,14 @@
+public class RemovingStars {
+
+    public String removeStars(String input) {
+
+        return "";
+    }
+    public static void main(String[] args){
+
+        RemovingStars test = new RemovingStars();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+}
