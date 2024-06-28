b9e9daebdf10d9a705db06753142d32dd6133e43
Raul Acosta <107330142+racodess@users.noreply.github.com>
2024-06-28 18:17:23 -0400

    Add solution for Queue problem

--- grokking-data-structures/src/StackUsingQueue.java
+++ grokking-data-structures/src/StackUsingQueue.java
@@ -1,12 +1,29 @@
 import java.util.Queue;
+import java.util.LinkedList;
 
 public class StackUsingQueue {
-    public void push(int x){
+    Queue<Integer> q = new LinkedList<>();
+    Queue<Integer> stkq = new LinkedList<>();
 
+    public void push(int x){
+        q.add(x);
     }
 
     public int pop(){
+        if (!q.isEmpty()){
+            while (!q.isEmpty()){
+                for (int j = 0; j < q.size() - 1; j++) {
+                    q.add(q.remove());
+                }
 
-        return -1;
+                stkq.add(q.remove());
+            }
+
+            int initialSize = stkq.size();
+            for (int i = 0; i < initialSize; i++)
+                stkq.add(stkq.remove());
+        }
+
+        return stkq.poll();
     }
 }
