2d6db4ea30e5e8c52c14a34ba05c740dbe79ba13
Raul Acosta <107330142+racodess@users.noreply.github.com>
2024-06-29 18:57:08 -0400

    Add solution template

---
+++ grokking-data-structures/src/ZigZagIterator.java
@@ -0,0 +1,16 @@
+public class ZigZagIterator {
+
+    public Solution(List<Integer> v1, List<Integer> v2) {
+
+    }
+
+    public int next() {
+
+        return -1;
+    }
+
+    public boolean hasNext() {
+
+        return false;
+    }
+}
