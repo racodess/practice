1a0ea1e9d6fb24262a00e6087bd7e79d2a8c0733
Raul Acosta <107330142+racodess@users.noreply.github.com>
2024-06-29 18:56:50 -0400

    Add solution for queue problem

--- grokking-data-structures/src/PalindromeCheck.java
+++ grokking-data-structures/src/PalindromeCheck.java
@@ -1,6 +1,32 @@
-public class PalindromeCheck {
-    public static boolean checkPalindrome(String s) {
+import java.util.Deque;
+import java.util.LinkedList;
 
-        return false;
+public class PalindromeCheck {
+    public static boolean checkPalindrome(String input) {
+        Deque<Character> deq = new LinkedList<Character>();
+
+        String str = "";
+        for (String s : input.trim().toLowerCase().split(" "))
+            str += s;
+
+        for (char c : str.toCharArray())
+            deq.add(c);
+
+        while (!deq.isEmpty()) {
+            if (deq.peekFirst() == deq.peekLast()) {
+                deq.pollFirst();
+                deq.pollLast();
+            }
+            else
+                return false;
+        }
+
+        return true;
+    }
+    public static void main(String[] args) {
+        PalindromeCheck test = new PalindromeCheck();
+
+        System.out.println(test.checkPalindrome("madam"));
+        System.out.println(test.checkPalindrome("A man a plan a canal Panama"));
     }
 }
