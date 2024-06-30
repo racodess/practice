fd0a375d8ab1c15c62f2800b8b5de064f6ce42b8
Raul Acosta <107330142+racodess@users.noreply.github.com>
2024-06-29 20:19:11 -0400

    Add solution to queue problem

--- grokking-data-structures/src/ZigZagIterator.java
+++ grokking-data-structures/src/ZigZagIterator.java
@@ -1,16 +1,41 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.List;
+import java.util.Arrays;
+
 public class ZigZagIterator {
+    Queue<Integer> q = new LinkedList<Integer>();
 
-    public Solution(List<Integer> v1, List<Integer> v2) {
+    public ZigZagIterator(List<Integer> v1, List<Integer> v2) {
 
+        int size = (v1.size() >= v2.size()) ? v1.size() : v2.size();
+        for (int i = 0; i < size; i++) {
+            if (i < v1.size())
+                q.add(v1.get(i));
+            if (i < v2.size())
+                q.add(v2.get(i));
+        }
     }
 
     public int next() {
-
-        return -1;
+        return q.poll();
     }
 
     public boolean hasNext() {
+        return !q.isEmpty();
+    }
 
-        return false;
+    public static void main(String[] args) {
+        ZigZagIterator i;
+        List<Integer> v1 = Arrays.asList(1, 2);
+        List<Integer> v2 = Arrays.asList(3, 4, 5, 6);
+
+        i = new ZigZagIterator(v1, v2);
+
+        while (i.hasNext()) {
+            System.out.println(i.next());
+        }
+
+        System.out.println(i.hasNext());
     }
 }
