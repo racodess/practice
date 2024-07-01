00e9e3ee122d8c2b80b95fba87b76264a88b4291
Raul Acosta <107330142+racodess@users.noreply.github.com>
2024-07-01 19:56:35 -0400

    Add solution template

---
+++ grokking-data-structures/src/DoublyPalindrome.java
@@ -0,0 +1,13 @@
+// class DLNode {
+//     int val;
+//     DLNode next, prev;
+//     DLNode(int val) { this.val = val; }
+// }
+
+public class DoublyPalindrome {
+
+    public boolean isPalindrome(DLNode head) {
+
+        return false;
+    }
+}
