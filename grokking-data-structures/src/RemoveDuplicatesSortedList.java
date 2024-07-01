a5b1c7b148ce5c06ac8669cd142dc627b10cc7fd
Raul Acosta <107330142+racodess@users.noreply.github.com>
2024-07-01 16:20:39 -0400

    Add solution to Linked List problem

--- grokking-data-structures/src/RemoveDuplicatesSortedList.java
+++ grokking-data-structures/src/RemoveDuplicatesSortedList.java
@@ -2,6 +2,18 @@ public class RemoveDuplicatesSortedList {
 
     public ListNode deleteDuplicates(ListNode head) {
 
+        ListNode current = head;
+        ListNode next = current.next;
+        while (next != null) {
+            if (next.val == current.val) {
+                next = current.next = next.next;
+            }
+            else {
+                current = next;
+                next = current.next;
+            }
+        }
+
         return head;
     }
 }
