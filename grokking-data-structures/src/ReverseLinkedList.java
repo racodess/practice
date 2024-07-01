979693a6f9036fd674b7f1ad6b3cba9a4c6f9b4c
Raul Acosta <107330142+racodess@users.noreply.github.com>
2024-07-01 15:44:59 -0400

    Add solution to Linked List problem

--- grokking-data-structures/src/ReverseLinkedList.java
+++ grokking-data-structures/src/ReverseLinkedList.java
@@ -2,6 +2,22 @@ public class ReverseLinkedList {
 
     public ListNode reverseList(ListNode head) {
 
-        return head;
+        ListNode ptr1 = head;
+        ListNode ptr2 = ptr1.next;
+        ListNode tmp = head;
+        for (int i = 0; ptr2 != null && ptr2.next != null; i++) {
+            tmp = ptr2.next;
+            ptr2.next = ptr1;
+            ptr1 = ptr2;
+            ptr2 = tmp;
+        }
+
+        if (ptr2 == null)
+            return head;
+
+        ptr2.next = ptr1;
+        head.next = null;
+
+        return ptr2;
     }
 }
