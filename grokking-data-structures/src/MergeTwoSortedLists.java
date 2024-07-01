a0aed08ebb03fd97ddac4d628f82d4d744ba8c0a
Raul Acosta <107330142+racodess@users.noreply.github.com>
2024-07-01 19:42:45 -0400

    Add solution to Linked List problem

--- grokking-data-structures/src/MergeTwoSortedLists.java
+++ grokking-data-structures/src/MergeTwoSortedLists.java
@@ -2,6 +2,24 @@ public class MergeTwoSortedLists {
 
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
 
-        return l1;
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+
+        ListNode current = (l1.val < l2.val) ? l1 : l2;
+        ListNode head = current;
+        ListNode prev = current;
+        ListNode other = (l1.val < l2.val) ? l2 : l1;
+        while (current.next != null) {
+            if (current.next.val > other.val) {
+                current = other;
+                other = prev.next;
+                prev = prev.next = current;
+            }
+            else
+                prev = current = current.next;
+        }
+        current.next = other;
+
+        return head;
     }
 }
