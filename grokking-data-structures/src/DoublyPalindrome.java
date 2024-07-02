c765d2a94aa38498a6d1b99c18b83f6bfc80049e
Raul Acosta <107330142+racodess@users.noreply.github.com>
2024-07-01 21:14:41 -0400

    Add solution to Linked List problem

--- grokking-data-structures/src/DoublyPalindrome.java
+++ grokking-data-structures/src/DoublyPalindrome.java
@@ -7,7 +7,21 @@
 public class DoublyPalindrome {
 
     public boolean isPalindrome(DLNode head) {
+        DLNode dummy = new DLNode(-1);
+        dummy.next = head;
 
-        return false;
+        while (dummy.next != null) {
+            dummy.prev = dummy.next;
+            dummy.next = dummy.next.next;
+        }
+
+        while (head != null) {
+            if (head.val != dummy.prev.val)
+                return false;
+            head = head.next;
+            dummy.prev = dummy.prev.prev;
+        }
+
+        return true;
     }
 }
